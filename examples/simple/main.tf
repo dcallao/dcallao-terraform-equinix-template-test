302a1d470d7c5a8f13d8b684b6fe1537bd986656
dcallao <dcallao@users.noreply.github.com>
2023-02-06 14:46:01 -0800

    Initial commit

---
+++ examples/simple/main.tf
@@ -0,0 +1,8 @@
+module "example" {
+  # TEMPLATE: Replace this path with the Git repo path or Terraform Registry path
+  source = "../../"
+
+  # Define any required variables
+  metal_project_id = var.example_metal_project_id
+  metal_auth_token = var.example_metal_auth_token
+}
